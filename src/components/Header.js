5bad1e94f2513e9be3540266755acae469f464f1
Anthony Calo <acalo4554@gmail.com>
2021-09-14 16:49:40 -0400

    Ignore eslint. And security error with a tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 
-
+/* eslint-disable */ 
 const Header =()=>{
     return(
            <div id="header" className="ui menu ac_header">
                <a href="/" className='item header_item'>AC Encryption</a>
                <div className="right menu">
-               <a href="https://github.com/paulie-walnuts/FileEncryption_Browser" target="_blank"><img alt="github"  className="gitLogo" src="/images/git_logo.png" /></a>
+               <a href="https://github.com/paulie-walnuts/FileEncryption_Browser" target="_blank" rel="noreferrer"><img alt="github"  className="gitLogo" src="/images/git_logo.png" /></a>
                </div>
            </div>
        )
